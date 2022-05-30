e00b8e941d97eb756bd882dd01071ec4a1e4c304
ludmilagrineva <102857691+miillli@users.noreply.github.com>
2022-05-30 10:58:00 +0300

    Initial commit

---
+++ src/main/java/Parser.java
@@ -0,0 +1,63 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Parser {
+    private static Document getPage() throws IOException {
+        String url="https://www.pogoda.spb.ru/";
+        Document page= Jsoup.parse(new URL(url), 6000);
+        return page;
+    }
+    private static Pattern pattern=Pattern.compile("\\d{2}\\.\\d{2}");
+
+    private static String getDateFromString(String stringDate) throws Exception {
+        Matcher matcher=pattern.matcher(stringDate);
+        if(matcher.find()){
+            return matcher.group();
+        }
+        throw new Exception("Can't extract date from string");
+    }
+
+    private static int printForValues(Elements values, int index) {
+        int iterationCount=4;
+        if (index == 0) {
+            Element valueLn = values.get(3);
+            boolean isMorning = valueLn.text().contains("Утро");
+            if (isMorning) {
+                iterationCount = 3;
+            }
+        }
+            for (int i = 0; i < iterationCount; i++) {
+                Element valueLine = values.get(index + i);
+                for (Element td : valueLine.select("td")) {
+                    System.out.print(td.text() + "    ");
+                }
+                System.out.println();
+            }
+            return iterationCount;
+        }
+
+    public static void main(String[] args) throws Exception {
+        Document page=getPage();
+        Element tableWeather=page.select("table[class=wt]").first();
+        Elements names=tableWeather.select("tr[class=wth]");
+        Elements values=tableWeather.select("tr[valign=top]");
+        int index=0;
+
+        for(Element name:names) {
+            String dateString = name.select("th[id=dt]").text();
+            String date = getDateFromString(dateString);
+            System.out.println(date + "      Явления      Температура      Давление      Влажность      Ветер");
+
+           int iterationCount= printForValues(values, index);
+           index=index+iterationCount;
+        }
+    }
+}
